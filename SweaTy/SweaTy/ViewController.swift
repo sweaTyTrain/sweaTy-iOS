ccb9cbe571a4454e4b3daae63619c361b763751a
이대현 <ej4658@naver.com>
2024-02-18 14:35:29 +0900

    [Feat] 프로젝트 세팅

---
+++ SweaTy/SweaTy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SweaTy
+//
+//  Created by 이대현 on 2/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
