a1ad6699c71f68bda01fc4dedffcc7a9616dfd3b
이대현 <ej4658@naver.com>
2024-03-28 00:01:08 +0900

    [Feat] 웹뷰 추가

--- SweaTy/SweaTy/Sources/Home/VCs/ExercisingViewController.swift
+++ SweaTy/SweaTy/Sources/Home/VCs/ExercisingViewController.swift
@@ -7,12 +7,15 @@
 
 import SnapKit
 import UIKit
+import WebKit
 
 protocol ExercisingViewControllerDelegate: AnyObject {
     func moveResultView()
 }
 
 final class ExercisingViewController: UIViewController {
+    private var webView = WKWebView()
+    
     private lazy var finishButton: UIButton = {
         var config = UIButton.Configuration.filled()
         config.background.backgroundColor = .red
@@ -30,18 +33,38 @@ final class ExercisingViewController: UIViewController {
     
     weak var delegate: ExercisingViewControllerDelegate? = nil
     
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        self.webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        self.view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         self.setupUI()
+        self.setupURL()
     }
     
     private func setupUI() {
+//        view.addSubview(webView)
+//        webView.snp.makeConstraints { make in
+//            make.edges.equalTo(view.safeAreaLayoutGuide)
+//        }
+        
         view.addSubview(finishButton)
         finishButton.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+            make.left.equalTo(view.safeAreaLayoutGuide).offset(30)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-30)
             make.height.equalTo(50)
             make.width.equalTo(100)
+        }
+    }
+    
+    private func setupURL() {
+        if let url = URL(string: "https://www.apple.com") {
+            let request = URLRequest(url: url)
+            webView.load(request)
         }
     }
 }
